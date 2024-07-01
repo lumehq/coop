bfda50b348562ec2924bec8ad29b0fa1119c91f9
reya <123083837+reyamir@users.noreply.github.com>
2024-07-01 17:21:44 +0700

    chore: setup project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use gpui::*;
+
+struct HelloWorld {
+    text: SharedString,
+}
+
+impl Render for HelloWorld {
+    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
+        div()
+            .size_full()
+            .flex()
+            .justify_center()
+            .items_center()
+            .text_sm()
+            .text_color(rgb(0xffffff))
+            .child(format!("hello, {}!", &self.text))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new().run(|cx: &mut AppContext| {
+        let bounds = Bounds::centered(None, size(px(800.0), px(600.0)), cx);
+        cx.open_window(
+            WindowOptions {
+                titlebar: Some(TitlebarOptions {
+                    title: Some("coop".into()),
+                    appears_transparent: true,
+                    traffic_light_position: None,
+                }),
+                window_bounds: Some(WindowBounds::Windowed(bounds)),
+                ..Default::default()
+            },
+            |cx| {
+                cx.new_view(|_cx| HelloWorld {
+                    text: "coop!".into(),
+                })
+            },
+        )
+        .unwrap();
+    });
 }
