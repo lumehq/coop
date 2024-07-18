6d00ea548aaf4fe1862eb3868ca85e23b88c715b
XIAO YU <xyzmhx@gmail.com>
2024-07-18 09:12:40 +0900

    Refactor error handling to use anyhow and propagate errors using ? operator (#1)

--- src/ui/chats.rs
+++ src/ui/chats.rs
@@ -2,11 +2,13 @@ use dioxus_router::prelude::{Outlet, Routable};
 use freya::prelude::*;
 use nostr_sdk::prelude::*;
 
-use crate::system::state::{CHATS, get_client, INBOXES, MESSAGES};
+use crate::system::state::{get_client, CHATS, INBOXES, MESSAGES};
 use crate::theme::{COLORS, GRID_ICON, PLUS_ICON};
-use crate::ui::components::{Direction, Divider};
-use crate::ui::components::chat::{ChannelForm, ChannelList, ChannelMembers, Messages, NewMessagePopup};
+use crate::ui::components::chat::{
+    ChannelForm, ChannelList, ChannelMembers, Messages, NewMessagePopup,
+};
 use crate::ui::components::user::CurrentUser;
+use crate::ui::components::{Direction, Divider};
 
 #[derive(Routable, Clone, PartialEq)]
 #[rustfmt::skip]
@@ -23,41 +25,41 @@ pub enum Chats {
 
 #[component]
 fn Main() -> Element {
-	let grid_icon = static_bytes(GRID_ICON);
-	let plus_icon = static_bytes(PLUS_ICON);
+    let grid_icon = static_bytes(GRID_ICON);
+    let plus_icon = static_bytes(PLUS_ICON);
 
-	let mut show_popup = use_signal(|| false);
-	let mut future = use_future(move || async move {
-		let client = get_client().await;
-		client
-			.handle_notifications(|notification| async {
-				if let RelayPoolNotification::Event { event, .. } = notification {
-					if event.kind == Kind::GiftWrap {
-						if let Ok(UnwrappedGift { rumor, sender }) =
-							client.unwrap_gift_wrap(&event).await
-						{
-							let chats = CHATS.read().iter().map(|ev| ev.pubkey).collect::<Vec<_>>();
+    let mut show_popup = use_signal(|| false);
+    let mut future = use_future(move || async move {
+        let client = get_client().await;
+        client
+            .handle_notifications(|notification| async {
+                if let RelayPoolNotification::Event { event, .. } = notification {
+                    if event.kind == Kind::GiftWrap {
+                        if let Ok(UnwrappedGift { rumor, sender }) =
+                            client.unwrap_gift_wrap(&event).await
+                        {
+                            let chats = CHATS.read().iter().map(|ev| ev.pubkey).collect::<Vec<_>>();
 
-							if chats.iter().any(|pk| pk == &sender) {
-								MESSAGES.write().push(rumor);
-							} else {
-								CHATS.write().push(rumor);
-							}
-						}
-					}
-				}
-				Ok(false)
-			})
-			.await
-			.expect("TODO: panic message");
-	});
+                            if chats.iter().any(|pk| pk == &sender) {
+                                MESSAGES.write().push(rumor);
+                            } else {
+                                CHATS.write().push(rumor);
+                            }
+                        }
+                    }
+                }
+                Ok(false)
+            })
+            .await
+            .expect("TODO: panic message");
+    });
 
-	use_drop(move || {
-		future.cancel();
-	});
+    use_drop(move || {
+        future.cancel();
+    });
 
-	rsx!(
-		NewMessagePopup { show_popup }
+    rsx!(
+        NewMessagePopup { show_popup }
         rect {
             content: "fit",
             height: "100%",
@@ -66,77 +68,77 @@ fn Main() -> Element {
                 width: "280",
                 height: "100%",
                 direction: "vertical",
-				rect {
-					height: "calc(100% - 45)",
-					padding: "0 8",
-					rect {
-						width: "100%",
-						height: "44",
-						direction: "horizontal",
-						main_align: "space-between",
-						cross_align: "center",
-						rect {}
-						rect {
-							direction: "horizontal",
-							main_align: "center",
-							cross_align: "center",
-							Link {
-								to: Chats::Welcome,
-								rect {
-		                            width: "24",
-		                            height: "24",
-		                            main_align: "center",
-		                            cross_align: "center",
-									margin: "0 8 0 0",
-		                            svg {
-		                                width: "16",
-		                                height: "16",
-		                                svg_data: grid_icon,
-		                            }
-		                        }
-							}
-							Button {
-				                onpress: move |_| show_popup.set(true),
-				                theme: Some(ButtonThemeWith {
-		                            background: Some(Cow::Borrowed(COLORS.neutral_200)),
-		                            hover_background: Some(Cow::Borrowed(COLORS.neutral_400)),
-		                            border_fill: Some(Cow::Borrowed(COLORS.neutral_200)),
-		                            focus_border_fill: Some(Cow::Borrowed(COLORS.neutral_200)),
-		                            corner_radius: Some(Cow::Borrowed("24 8 24 24")),
-		                            font_theme: Some(FontThemeWith {
-		                                color: Some(Cow::Borrowed(COLORS.black)),
-		                            }),
-		                            width: Some(Cow::Borrowed("44")),
-		                            height: Some(Cow::Borrowed("24")),
-		                            margin: Some(Cow::Borrowed("0")),
-		                            padding: Some(Cow::Borrowed("0")),
-		                            shadow: Some(Cow::Borrowed("none")),
-		                        }),
-		                        rect {
-		                            width: "44",
-		                            height: "24",
-		                            corner_radius: "24 8 24 24",
-		                            main_align: "center",
-		                            cross_align: "center",
-		                            svg {
-		                                width: "16",
-		                                height: "16",
-		                                svg_data: plus_icon,
-		                            }
-		                        }
-				            }
-						}
-					}
-	                ChannelList {},
-				}
-                Divider { background: COLORS.neutral_200, direction: Direction::HORIZONTAL },
+                rect {
+                    height: "calc(100% - 45)",
+                    padding: "0 8",
+                    rect {
+                        width: "100%",
+                        height: "44",
+                        direction: "horizontal",
+                        main_align: "space-between",
+                        cross_align: "center",
+                        rect {}
+                        rect {
+                            direction: "horizontal",
+                            main_align: "center",
+                            cross_align: "center",
+                            Link {
+                                to: Chats::Welcome,
+                                rect {
+                                    width: "24",
+                                    height: "24",
+                                    main_align: "center",
+                                    cross_align: "center",
+                                    margin: "0 8 0 0",
+                                    svg {
+                                        width: "16",
+                                        height: "16",
+                                        svg_data: grid_icon,
+                                    }
+                                }
+                            }
+                            Button {
+                                onpress: move |_| show_popup.set(true),
+                                theme: Some(ButtonThemeWith {
+                                    background: Some(Cow::Borrowed(COLORS.neutral_200)),
+                                    hover_background: Some(Cow::Borrowed(COLORS.neutral_400)),
+                                    border_fill: Some(Cow::Borrowed(COLORS.neutral_200)),
+                                    focus_border_fill: Some(Cow::Borrowed(COLORS.neutral_200)),
+                                    corner_radius: Some(Cow::Borrowed("24 8 24 24")),
+                                    font_theme: Some(FontThemeWith {
+                                        color: Some(Cow::Borrowed(COLORS.black)),
+                                    }),
+                                    width: Some(Cow::Borrowed("44")),
+                                    height: Some(Cow::Borrowed("24")),
+                                    margin: Some(Cow::Borrowed("0")),
+                                    padding: Some(Cow::Borrowed("0")),
+                                    shadow: Some(Cow::Borrowed("none")),
+                                }),
+                                rect {
+                                    width: "44",
+                                    height: "24",
+                                    corner_radius: "24 8 24 24",
+                                    main_align: "center",
+                                    cross_align: "center",
+                                    svg {
+                                        width: "16",
+                                        height: "16",
+                                        svg_data: plus_icon,
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    ChannelList {},
+                }
+                Divider { background: COLORS.neutral_200, direction: Direction::Horizontal },
                 rect {
                     width: "100%",
                     height: "44",
                     CurrentUser {}
                 }
             }
-            Divider { background: COLORS.neutral_250, direction: Direction::VERTICAL },
+            Divider { background: COLORS.neutral_250, direction: Direction::Vertical },
             rect {
                 width: "fill-min",
                 height: "100%",
@@ -149,7 +151,7 @@ fn Main() -> Element {
 
 #[component]
 fn Welcome() -> Element {
-	rsx!(
+    rsx!(
         rect {
             width: "100%",
             height: "100%",
@@ -164,18 +166,18 @@ fn Welcome() -> Element {
 
 #[component]
 fn Channel(hex: ReadOnlySignal<String>) -> Element {
-	let sender = PublicKey::from_hex(hex.read().to_string()).unwrap();
+    let sender = PublicKey::from_hex(hex.read().to_string()).unwrap();
 
-	let inbox = use_memo(use_reactive((&sender, &INBOXES()), |(sender, inboxes)| {
-		inboxes.get(&sender).cloned()
-	}));
+    let inbox = use_memo(use_reactive((&sender, &INBOXES()), |(sender, inboxes)| {
+        inboxes.get(&sender).cloned()
+    }));
 
-	let scroll_controller = use_scroll_controller(|| ScrollConfig {
-		default_vertical_position: ScrollPosition::End,
-		..Default::default()
-	});
+    let scroll_controller = use_scroll_controller(|| ScrollConfig {
+        default_vertical_position: ScrollPosition::End,
+        ..Default::default()
+    });
 
-	rsx!(
+    rsx!(
         rect {
             width: "100%",
             height: "100%",
@@ -190,7 +192,7 @@ fn Channel(hex: ReadOnlySignal<String>) -> Element {
                     ChannelMembers { sender }
                 }
             },
-            Divider { background: COLORS.neutral_200, direction: Direction::HORIZONTAL },
+            Divider { background: COLORS.neutral_200, direction: Direction::Horizontal },
             rect {
                 height: "calc(100% - 89)",
                 ScrollView {
@@ -206,30 +208,30 @@ fn Channel(hex: ReadOnlySignal<String>) -> Element {
             },
             match inbox() {
                 Some(relays) => {
-					match relays.is_empty() {
-						false => rsx!( ChannelForm { sender, relays } ),
-						true => rsx!(
-		                    rect {
-		                        width: "100%",
-		                        height: "44",
-		                        main_align: "center",
-		                        cross_align: "center",
-		                        rect {
-		                            height: "28",
-		                            background: COLORS.neutral_100,
-		                            padding: "4 12",
-		                            corner_radius: "28",
-		                            main_align: "center",
-		                            cross_align: "center",
-		                            label {
-		                                font_size: "11",
-										"This user isn't have inbox relays, you cannot send message."
-		                            }
-		                        }
-		                    }
-		                )
-					}
-				},
+                    match relays.is_empty() {
+                        false => rsx!( ChannelForm { sender, relays } ),
+                        true => rsx!(
+                            rect {
+                                width: "100%",
+                                height: "44",
+                                main_align: "center",
+                                cross_align: "center",
+                                rect {
+                                    height: "28",
+                                    background: COLORS.neutral_100,
+                                    padding: "4 12",
+                                    corner_radius: "28",
+                                    main_align: "center",
+                                    cross_align: "center",
+                                    label {
+                                        font_size: "11",
+                                        "This user isn't have inbox relays, you cannot send message."
+                                    }
+                                }
+                            }
+                        )
+                    }
+                },
                 None => rsx!(
                     rect {
                         width: "100%",
@@ -245,7 +247,7 @@ fn Channel(hex: ReadOnlySignal<String>) -> Element {
                             cross_align: "center",
                             label {
                                 font_size: "11",
-								"Connecting to inbox relays..."
+                                "Connecting to inbox relays..."
                             }
                         }
                     }
@@ -257,5 +259,5 @@ fn Channel(hex: ReadOnlySignal<String>) -> Element {
 
 #[component]
 fn NotFound() -> Element {
-	rsx!(rect {})
+    rsx!(rect {})
 }
