8ae1368419dc9dc230805f0a31b79a65a09dbf5f
reya <123083837+reyamir@users.noreply.github.com>
2024-07-23 13:28:45 +0700

    chore: setup ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import "./app.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+// Import the generated route tree
+import { routeTree } from "./routes.gen";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+// Render the app
+const rootElement = document.getElementById("root") as HTMLElement;
+if (!rootElement.innerHTML) {
+	const root = ReactDOM.createRoot(rootElement);
+	root.render(
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>,
+	);
+}
