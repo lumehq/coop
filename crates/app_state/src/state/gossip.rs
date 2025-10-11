0f005c0201e3b5c5032f3bfe99e5d7d6365d867d
reya <reya@lume.nu>
2025-10-11 19:59:58 +0700

    add set nip17 and set nip65 functions

--- crates/app_state/src/state/gossip.rs
+++ crates/app_state/src/state/gossip.rs
@@ -15,10 +15,12 @@ pub struct Gossip {
 }
 
 impl Gossip {
+    /// Parse and insert NIP-65 or NIP-17 relays into the gossip state.
     pub fn insert(&mut self, event: &Event) {
         match event.kind {
             Kind::InboxRelays => {
-                let urls: Vec<RelayUrl> = nip17::extract_relay_list(event).cloned().collect();
+                let urls: Vec<RelayUrl> =
+                    nip17::extract_relay_list(event).take(3).cloned().collect();
 
                 if !urls.is_empty() {
                     self.nip17.entry(event.pubkey).or_default().extend(urls);
@@ -37,6 +39,7 @@ impl Gossip {
         }
     }
 
+    /// Get all write relays for a given public key
     pub fn write_relays(&self, public_key: &PublicKey) -> Vec<&RelayUrl> {
         self.nip65
             .get(public_key)
@@ -51,6 +54,7 @@ impl Gossip {
             .unwrap_or_default()
     }
 
+    /// Get all read relays for a given public key
     pub fn read_relays(&self, public_key: &PublicKey) -> Vec<&RelayUrl> {
         self.nip65
             .get(public_key)
@@ -65,6 +69,7 @@ impl Gossip {
             .unwrap_or_default()
     }
 
+    /// Get all messaging relays for a given public key
     pub fn messaging_relays(&self, public_key: &PublicKey) -> Vec<&RelayUrl> {
         self.nip17
             .get(public_key)
@@ -72,6 +77,9 @@ impl Gossip {
             .unwrap_or_default()
     }
 
+    /// Get and verify NIP-65 relays for a given public key
+    ///
+    /// Only fetch from the public relays
     pub async fn get_nip65(&self, public_key: PublicKey) -> Result<(), Error> {
         let client = nostr_client();
         let timeout = Duration::from_secs(5);
@@ -103,6 +111,33 @@ impl Gossip {
         Ok(())
     }
 
+    /// Set NIP-65 relays for a current user
+    pub async fn set_nip65(
+        &mut self,
+        relays: Vec<(RelayUrl, Option<RelayMetadata>)>,
+    ) -> Result<(), Error> {
+        let client = nostr_client();
+        let signer = client.signer().await?;
+
+        let tags: Vec<Tag> = relays
+            .into_iter()
+            .map(|(url, metadata)| Tag::relay_metadata(url, metadata))
+            .collect();
+
+        let event = EventBuilder::new(Kind::RelayList, "")
+            .tags(tags)
+            .sign(&signer)
+            .await?;
+
+        // Send event to the public relays
+        client.send_event_to(BOOTSTRAP_RELAYS, &event).await?;
+
+        Ok(())
+    }
+
+    /// Get and verify NIP-17 relays for a given public key
+    ///
+    /// Only fetch from public key's write relays
     pub async fn get_nip17(&self, public_key: PublicKey) -> Result<(), Error> {
         let client = nostr_client();
         let timeout = Duration::from_secs(5);
@@ -147,6 +182,39 @@ impl Gossip {
         Ok(())
     }
 
+    /// Set NIP-17 relays for a current user
+    pub async fn set_nip17(&mut self, relays: Vec<RelayUrl>) -> Result<(), Error> {
+        let client = nostr_client();
+        let signer = client.signer().await?;
+        let public_key = signer.get_public_key().await?;
+
+        let urls = self.write_relays(&public_key);
+
+        // Ensure user's have at least one relay
+        if urls.is_empty() {
+            return Err(anyhow!("Relays are empty"));
+        }
+
+        // Ensure connection to relays
+        for url in urls.iter().cloned() {
+            client.add_relay(url).await?;
+            client.connect_relay(url).await?;
+        }
+
+        let event = EventBuilder::new(Kind::InboxRelays, "")
+            .tags(relays.into_iter().map(Tag::relay))
+            .sign(&signer)
+            .await?;
+
+        // Send event to the public relays
+        client.send_event_to(urls, &event).await?;
+
+        Ok(())
+    }
+
+    /// Subscribe for events that match the given kind for a given author
+    ///
+    /// Only fetch from author's write relays
     pub async fn subscribe(&self, public_key: PublicKey, kind: Kind) -> Result<(), Error> {
         let client = nostr_client();
         let opts = SubscribeAutoCloseOptions::default().exit_policy(ReqExitPolicy::ExitOnEOSE);
@@ -171,6 +239,9 @@ impl Gossip {
         Ok(())
     }
 
+    /// Bulk subscribe to metadata events for a list of public keys
+    ///
+    /// Only fetch from the public relays
     pub async fn bulk_subscribe(&self, public_keys: HashSet<PublicKey>) -> Result<(), Error> {
         if public_keys.is_empty() {
             return Err(anyhow!("You need at least one public key"));
@@ -215,6 +286,7 @@ impl Gossip {
         Ok(())
     }
 
+    /// Send an event to author's write relays
     pub async fn send_event_to_write_relays(&self, event: &Event) -> Result<(), Error> {
         let client = nostr_client();
         let public_key = event.pubkey;
