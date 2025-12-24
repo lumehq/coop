5615def65e4f834fa684eb8d07c3976e4238ceea
reya <reya@lume.nu>
2025-12-24 18:54:28 +0700

    add flexoki and spaceduck themes

--- crates/theme/src/lib.rs
+++ crates/theme/src/lib.rs
@@ -122,7 +122,7 @@ impl Theme {
         M: Into<ThemeMode>,
     {
         if !cx.has_global::<Theme>() {
-            let default_theme = ThemeFamily::from_assets("catppuccin-frappe").unwrap();
+            let default_theme = ThemeFamily::from_assets("spaceduck").unwrap();
             let theme = Theme::from(default_theme);
 
             cx.set_global(theme);
